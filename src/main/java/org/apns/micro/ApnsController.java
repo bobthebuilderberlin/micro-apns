152e382a5a8ff874c78d89e243877b7113b41427
Robert Andruschow <robert.andruschow@kaufda.de>
2016-06-27 13:23:05 +0200

    - optimized imports

--- src/main/java/org/apns/micro/ApnsController.java
+++ src/main/java/org/apns/micro/ApnsController.java
@@ -2,8 +2,11 @@ package org.apns.micro;
 
 import org.apns.micro.core.ApnsManagement;
 import org.apns.micro.core.ApnsManagementService;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
 import java.util.Map;
